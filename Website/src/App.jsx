a2300090419e62ff97fa468ef71dd17e360502b2
mbila227 <muhammadbil27@gmail.com>
2024-07-25 01:40:58 -0400

    Finish

---
+++ Website/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Hero from './sections/Hero/Hero';
+import Contact from './sections/Contact/Contact';
+import Skills from './sections/Skills/Skills';
+import Footer from './sections/Footer/Footer';
+
+function App() {
+  return ( 
+  <>
+  <Hero />
+  <Skills />
+  <Contact />
+  <Footer />
+
+  
+  </>
+  );
+}
+
+export default App
